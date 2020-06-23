13fad723ed2b17692213bb34e86c8c5224047028
mhsvieira <mhsvieira@outlook.com>
2020-06-23 16:12:23 -0300

    commit inicial do projeto

---
+++ MSDNDemo/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace MSDNDemo
+{
+    partial class frmMSDNDemo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvMSDNDemo = new System.Windows.Forms.DataGridView();
+            this.btnAtualizaLista = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAnterior = new System.Windows.Forms.Button();
+            this.btnProxima = new System.Windows.Forms.Button();
+            this.txtPesquisa = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Título = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Postagem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Exibições = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Respostas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Link = new System.Windows.Forms.DataGridViewLinkColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMSDNDemo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMSDNDemo
+            // 
+            this.dgvMSDNDemo.AllowUserToAddRows = false;
+            this.dgvMSDNDemo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMSDNDemo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Título,
+            this.Postagem,
+            this.Exibições,
+            this.Respostas,
+            this.Link});
+            this.dgvMSDNDemo.Location = new System.Drawing.Point(12, 59);
+            this.dgvMSDNDemo.Name = "dgvMSDNDemo";
+            this.dgvMSDNDemo.Size = new System.Drawing.Size(914, 369);
+            this.dgvMSDNDemo.TabIndex = 0;
+            this.dgvMSDNDemo.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMSDNDemo_CellClick);
+            // 
+            // btnAtualizaLista
+            // 
+            this.btnAtualizaLista.Location = new System.Drawing.Point(398, 16);
+            this.btnAtualizaLista.Name = "btnAtualizaLista";
+            this.btnAtualizaLista.Size = new System.Drawing.Size(142, 23);
+            this.btnAtualizaLista.TabIndex = 1;
+            this.btnAtualizaLista.Text = "Atualizar Lista/Pesquisar";
+            this.btnAtualizaLista.UseVisualStyleBackColor = true;
+            this.btnAtualizaLista.Click += new System.EventHandler(this.btnAtualizaLista_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(839, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Página: ";
+            // 
+            // btnAnterior
+            // 
+            this.btnAnterior.Location = new System.Drawing.Point(564, 16);
+            this.btnAnterior.Name = "btnAnterior";
+            this.btnAnterior.Size = new System.Drawing.Size(75, 23);
+            this.btnAnterior.TabIndex = 4;
+            this.btnAnterior.Text = "Anterior";
+            this.btnAnterior.UseVisualStyleBackColor = true;
+            this.btnAnterior.Click += new System.EventHandler(this.btnAnterior_Click);
+            // 
+            // btnProxima
+            // 
+            this.btnProxima.Location = new System.Drawing.Point(664, 16);
+            this.btnProxima.Name = "btnProxima";
+            this.btnProxima.Size = new System.Drawing.Size(75, 23);
+            this.btnProxima.TabIndex = 5;
+            this.btnProxima.Text = "Próxima";
+            this.btnProxima.UseVisualStyleBackColor = true;
+            this.btnProxima.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtPesquisa
+            // 
+            this.txtPesquisa.Location = new System.Drawing.Point(76, 16);
+            this.txtPesquisa.Name = "txtPesquisa";
+            this.txtPesquisa.Size = new System.Drawing.Size(293, 20);
+            this.txtPesquisa.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Pesquisa:";
+            // 
+            // Título
+            // 
+            this.Título.HeaderText = "Título";
+            this.Título.Name = "Título";
+            this.Título.Width = 300;
+            // 
+            // Postagem
+            // 
+            this.Postagem.HeaderText = "Postagem";
+            this.Postagem.Name = "Postagem";
+            this.Postagem.Width = 150;
+            // 
+            // Exibições
+            // 
+            this.Exibições.HeaderText = "Exibições";
+            this.Exibições.Name = "Exibições";
+            // 
+            // Respostas
+            // 
+            this.Respostas.HeaderText = "Respostas";
+            this.Respostas.Name = "Respostas";
+            // 
+            // Link
+            // 
+            this.Link.HeaderText = "Link";
+            this.Link.Name = "Link";
+            this.Link.Width = 200;
+            // 
+            // frmMSDNDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 443);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPesquisa);
+            this.Controls.Add(this.btnProxima);
+            this.Controls.Add(this.btnAnterior);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAtualizaLista);
+            this.Controls.Add(this.dgvMSDNDemo);
+            this.Name = "frmMSDNDemo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Web Scraping MSDN Demo 1.0";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMSDNDemo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvMSDNDemo;
+        private System.Windows.Forms.Button btnAtualizaLista;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAnterior;
+        private System.Windows.Forms.Button btnProxima;
+        private System.Windows.Forms.TextBox txtPesquisa;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Título;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Postagem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Exibições;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Respostas;
+        private System.Windows.Forms.DataGridViewLinkColumn Link;
+    }
+}
+
---
+++ MSDNDemo/Form1.cs
@@ -0,0 +1,128 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Windows.Forms;
+
+namespace MSDNDemo
+{
+    public partial class frmMSDNDemo : Form
+    {
+        public frmMSDNDemo()
+        {
+            InitializeComponent();
+
+            btnAnterior.Enabled = false;
+            btnProxima.Enabled = false;
+        }
+
+        int pageNumber = 1;
+
+        // se a fonte de dados for um Webservice ou API, sempre optar por esses. Somente se não houver uma fonte padronizada, usar essa técnica
+        private void btnAtualizaLista_Click(object sender, EventArgs e)
+        {
+            Processa(1, txtPesquisa.Text);
+        }
+
+        private void dgvMSDNDemo_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 4) // coluna Link
+            {
+                Process.Start(new ProcessStartInfo(dgvMSDNDemo.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString()));
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pageNumber++;
+            Processa(pageNumber, txtPesquisa.Text);
+        }
+
+        private void btnAnterior_Click(object sender, EventArgs e)
+        {
+            pageNumber--;
+
+            if (pageNumber <= 0)
+                pageNumber = 1;
+
+            Processa(pageNumber, txtPesquisa.Text);
+        }
+
+        private void Processa(int pageNumber, string strPesquisa)
+        {
+            var wc = new WebClient();
+
+            wc.Encoding = System.Text.Encoding.UTF8;
+
+            string url = "https://social.msdn.microsoft.com/Forums/pt-BR/home?filter=alltypes&sort=firstpostdesc";
+
+            if (strPesquisa.Trim().Length > 0)
+            {
+                url = url + "&searchTerm=" + strPesquisa;
+            }
+
+            if (pageNumber > 1)
+            {
+                url = url + "&page=" + pageNumber.ToString();
+            }
+   
+            string pagina = wc.DownloadString(url);
+
+            if (pagina.Contains("Nenhum resultado encontrado"))
+            {
+                MessageBox.Show("Pesquisa não retornou resultado.");
+                return;
+            }
+
+            var htmlDocument = new HtmlAgilityPack.HtmlDocument();
+
+            htmlDocument.LoadHtml(pagina);
+
+            dgvMSDNDemo.Rows.Clear();
+
+            string id = string.Empty;
+            string titulo = string.Empty;
+            string postagem = string.Empty;
+            string exibicao = string.Empty;
+            string resposta = string.Empty;
+            string link = string.Empty;
+
+            try
+            {
+                foreach (HtmlNode node in htmlDocument.GetElementbyId("threadList").ChildNodes)
+                {
+                    if (node.Attributes.Count > 0)
+                    {
+                        id = node.Attributes["data-threadid"].Value;
+
+                        link = "https://social.msdn.microsoft.com/Forums/pt-BR/" + id;
+
+                        titulo = node.Descendants().First(x => x.Id.Equals("threadTitle_" + id)).InnerText;
+
+                        postagem = WebUtility.HtmlDecode(node.Descendants().First(x => x.Attributes["class"] != null && x.Attributes["class"].Value.Equals("lastpost")).InnerText.Replace("\n", "").Replace("  ", ""));
+
+                        exibicao = WebUtility.HtmlDecode(node.Descendants().First(x => x.Attributes["class"] != null && x.Attributes["class"].Value.Equals("viewcount")).InnerText);
+
+                        resposta = WebUtility.HtmlDecode(node.Descendants().First(x => x.Attributes["class"] != null && x.Attributes["class"].Value.Equals("replycount")).InnerText);
+
+                        if (!string.IsNullOrEmpty(titulo))
+                        {
+                            dgvMSDNDemo.Rows.Add(titulo, postagem, exibicao, resposta, link); // mesma ordem das colunas em tela
+                        }
+                    }
+                }
+            }
+            catch 
+            {
+                MessageBox.Show("Falha ao realizar a busca no site.");
+                return;
+            }
+
+            btnAnterior.Enabled = pageNumber > 1;
+            btnProxima.Enabled =  pageNumber > 0;
+
+            label1.Text = "Página: " + pageNumber.ToString();
+        }
+    }
+}
